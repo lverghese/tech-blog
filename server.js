fc7881857ebf8907f704d451724bcb2d92f7f3b0
lverghese <loganverghese@gmail.com>
2021-09-22 15:40:14 -0400

    fixed login route and added logout feature

--- server.js
+++ server.js
@@ -4,14 +4,25 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'nevagonna getdis 9356',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
