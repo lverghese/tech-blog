e3cb24c4303b39b9bdb582da8edf326bf1e24633
lverghese <loganverghese@gmail.com>
2021-09-22 18:33:48 -0400

    adding a new post and dashboard update

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -44,7 +44,7 @@
       console.log(password);
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
